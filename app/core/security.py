5a44432d73b9ecbca60ac30d6349cdd2357bf5e7
Sachin <1eyedipatch@gmail.com>
2026-02-03 19:19:04 +0530

    Return 401 for invalid API key

--- app/core/security.py
+++ app/core/security.py
@@ -31,7 +31,7 @@ async def verify_api_key(api_key: str = Security(api_key_header)) -> str:
     
     if api_key != settings.API_SECRET_KEY:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid API key.",
         )
     
